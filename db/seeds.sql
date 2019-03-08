401f7cea26d468ae8dc0484fed912e4ea00a5399
Shain Dholakiya <shainshain99@gmail.com>
2019-03-08 11:56:53 -0500

    initial site files

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Cheese Burger", true);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Chicken Burger", true);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Ham Burger", true);
