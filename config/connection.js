6f56970e031291690c91e008e3cf078ee61fe573
Shain Dholakiya <shainshain99@gmail.com>
2019-03-16 12:12:12 -0400

    initial site files

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,4 @@
 require('dotenv').config();
-// const mysql = require('mysql');
-
-// let connection = mysql.createConnection({
-//     host: process.env.host,
-//     user: process.env.user,
-//     password: process.env.password,
-//     database: process.env.database
-// });;
-
-// connection.connect();
-
-// module.exports = connection
 
 module.exports = {
     "development": {
