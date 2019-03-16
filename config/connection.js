cf3f57ce0a499b13aa288ce9ea2ad469f4327d34
Shain Dholakiya <shainshain99@gmail.com>
2019-03-16 14:42:49 -0400

    initial site files

--- config/connection.js
+++ config/connection.js
@@ -18,17 +18,17 @@ module.exports = {
     //   "dialect": "mysql"
     // },
     // "production": {
-        // host: "zj2x67aktl2o6q2n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-        // user: "ay7u4g9197bveocl",
-        // password: "j26ze6igbc104znw",
-        // database: "hocdq3nbrqz38ftp",
-        // port: 3306,
-        // dialect: "mysql"
-        "host": process.env.host,
-        "user": process.env.user,
-        "password": process.env.password,
-        "database": process.env.database,
+        host: "zj2x67aktl2o6q2n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        user: "ay7u4g9197bveocl",
+        password: "j26ze6igbc104znw",
+        database: "hocdq3nbrqz38ftp",
         port: 3306,
         dialect: "mysql"
+        // host: process.env.host,
+        // user: process.env.user,
+        // password: process.env.password,
+        // database: process.env.database,
+        // port: 3306,
+        // dialect: "mysql"
     // },
   }
