204891286392af982469613bcd77f2e69015fc2d
Shain Dholakiya <shainshain99@gmail.com>
2019-03-16 13:25:02 -0400

    heroku

--- config/connection.js
+++ config/connection.js
@@ -17,12 +17,12 @@ module.exports = {
     //   "port": 3306,
     //   "dialect": "mysql"
     // },
-    "production": {
-      "username": process.env.user,
-      "password": process.env.password,
-      "database": process.env.database,
-      "host": process.env.host,
+    // "production": {
+      "username": "ay7u4g9197bveocl",
+      "password": "zee4w2xnbznccyct",
+      "database": "hocdq3nbrqz38ftp",
+      "host": "zj2x67aktl2o6q2n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
       "port": 3306,
       "dialect": "mysql"
-    },
+    // },
   }
